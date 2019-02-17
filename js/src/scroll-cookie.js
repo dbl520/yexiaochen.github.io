1c0210a3a1ea99674342937ebe35f00b9177c8ee
yexiaochen <2277438436@qq.com>
2019-02-17 14:25:14 +0800

    Site updated: 2019-02-17 14:25:14

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Feb 17 2019 14:15:37 GMT+0800 (CST)
+// build time:Sun Feb 17 2019 14:25:10 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
